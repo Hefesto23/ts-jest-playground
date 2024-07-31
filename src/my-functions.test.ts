c3cb149483cdc75bd56a4f67b6d07fa87c4b3be6
Hefesto23 <vini.ra@outlook.com>
2024-07-31 04:04:10 -0300

    test: Spies, Mocks and stubs

--- src/my-functions.test.ts
+++ src/my-functions.test.ts
@@ -1,7 +1,7 @@
-import { toUpperCase } from './my-functions';
+import { calculateStringChar, executeCbWithMsg, MathOperations, toUpperCase } from './my-functions';
 describe('my-functions tests', () => {
   
-  describe('Função toUpperCase ', () => {
+  describe('Stubs e Triple AAA', () => {
     /* 
       O padrão de teste Triple A, também conhecido como Padrão de Teste Triple AAA, é uma estratégia de teste que segue os princípios do Desenvolvimento Orientado a Testes (TDD) e Desenvolvimento Orientado a Comportamento (BDD). É um framework para escrever testes que garante código de alta qualidade e fácil manutenção.
 
@@ -16,16 +16,95 @@ describe('my-functions tests', () => {
     */
     it('deve retornar uma string maiúscula', () => {
       // Triple AAA: Arrange, Act, Assert
-      // Arrange
+      // Arrange(Preparação)
       const sut = toUpperCase;
       const expected = 'TESTE'
 
-      // Act
+      // Act (Ação)
       const actual = sut('teste');
 
-      // Assert
+      // Assert (Asserções)
       expect(actual).toBe(expected);
     });
-   })
+       // ************ STUBS *************************
+   it('deve retornar contagem de caracteres de uma string', () => {
+       // Arrange
+       const expected = 4;
+       const sut = calculateStringChar;
+       // Stub objetos incompletos de teste
+       /* Um stub é um objeto simples que substitui um objeto complexo em um sistema para fins de teste. Em um ambiente de teste, você pode usar um stub para simular o comportamento de um objeto existente ou para fornecer uma interface de teste para um objeto que ainda não foi implementado. */
+       const someInfoStubs = {
+         lowercase: 'casa',
+         characters: ['c', 'a', 's', 'a'],
+       }
+       // Act
+       const actual = sut(someInfoStubs as any);
+       // Assert
+       expect(actual).toBe(expected);
+    });
+  })
   
+  describe('Mocks', () => {
+    // ************ Mocks *************************
+    // Os testes unitários devem ser independentes e para isso o valor dos 
+    // mocks deve ser reinicializado a cada teste
+    beforeEach(() => {
+      jest.clearAllMocks();
+    })
+
+    it('deve retornar a contagem de vezes que um mock foi chamado e o que foi chamado', () => {
+      /* O Mock simula seu comportamento verificando se um ou mais métodos foram ou não chamados, a ordem de chamadas destes métodos, se esses métodos foram chamados com os argumentos certos, e quantas vezes foram chamados. */
+      // Preparo
+      const myCbFunction = jest.fn();
+      
+      // Ação
+      executeCbWithMsg(myCbFunction);
+
+      // Asserções
+      expect(myCbFunction).toHaveBeenCalled();
+      expect(myCbFunction).toHaveBeenCalledTimes(1);
+      expect(myCbFunction).toHaveBeenCalledWith('Hello World');
+    });
+  });
+
+  describe('Spies', () => {
+    // ************ Spies *************************
+    /* Spies não é um conceito tão comum quanto Mocks ou Stubs. Spy pode ser definido como um tipo de combinação de um Mock com um Stub. Mas não apenas isso, spies também permitem gravar as chamadas para o método “espionado”, além de outras informações. 
+    Spies são utilizados para verificar o comportamento de MÉTODOS DE UMA CLASSE.
+    */
+    let sut: MathOperations; 
+    beforeEach(() => {
+      sut = new MathOperations();
+    })
+
+    afterEach(() => {
+      jest.clearAllMocks();
+    })  
+    it('Deve espiar a classe de adição', () => {
+      jest.spyOn(sut, 'add');
+
+      sut.add(21, 4);
+      sut.add(23, 5);
+
+      expect(sut.add).toHaveBeenCalledTimes(2);
+
+      expect(sut.add).toHaveBeenCalledWith(21, 4);
+      expect(sut.add).toHaveBeenCalledWith(23, 5);
+    })
+    
+    it('Deve espiar a classe de multiplicação', () => {
+      jest.spyOn(sut, 'multiply').mockImplementationOnce(() => 6);
+
+      const result = sut.multiply(1, 4); // 4
+      sut.multiply(2, 5); // 10
+      sut.multiply(2, 3); // 6
+      expect(sut.multiply).toHaveBeenCalledTimes(3);
+
+      expect(sut.multiply).toHaveBeenCalledWith(1, 4);
+      expect(sut.multiply).toHaveBeenCalledWith(2, 5);
+      expect(result).toBe(6);
+    })
+   })
+
+
 })
--- src/my-functions.ts
+++ src/my-functions.ts
@@ -1,3 +1,34 @@
 export function toUpperCase(str: string): string {
   return str.toUpperCase();
 }
+
+export type StringInfo = {
+    lowercase: string;
+    uppercase: string;
+    characters: string[];
+    extraInfo: Object | undefined;
+}
+
+export function calculateStringChar(obj: StringInfo): number {
+  return obj.characters.length;
+}
+
+export function executeCbWithMsg(cb): void {
+  const msg = 'Hello World';
+  cb(msg);
+}
+
+export class MathOperations {
+    public add(a: number, b: number): number {
+        return a + b;
+    }
+    public sub(a: number, b: number): number{
+        return a - b;
+    }
+    public multiply(a: number, b: number): number {
+        return a * b;
+    }
+    public divide(a: number, b: number): number {
+        return a / b;
+    }
+}
